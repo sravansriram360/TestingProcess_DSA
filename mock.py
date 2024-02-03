e6fefa724d5f86b0ecaca97642ba1ba9a2644e56
sravansriram360 <ssravan360@tamu.edu>
2024-02-03 07:53:57 -0600

    Automatic Testing Files

---
+++ mock.py
@@ -0,0 +1,72 @@
+import os
+import shutil 
+
+def copy_to_multiple_destinations(source_folder, destination_folders):
+    try:
+        for destination_folder in destination_folders:
+            for item in os.listdir(source_folder):
+                source_item = os.path.join(source_folder, item)
+                destination_item = os.path.join(destination_folder, item)
+
+                if os.path.isdir(source_item):
+                    # If the item is a directory, use shutil.copytree to copy it recursively
+                    shutil.copytree(source_item, destination_item, dirs_exist_ok=True)
+                else:
+                    # If the item is a file, use shutil.copy2 to copy it
+                    shutil.copy2(source_item, destination_item)
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def get_all_folders(folder_path):
+    try:
+        # Get a list of all items in the folder
+        all_items = os.listdir(folder_path)
+
+        # Filter out only the folders
+        folders = [item for item in all_items if os.path.isdir(os.path.join(folder_path, item)) and not item == 'solution']
+
+        return folders
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+folder_path = './'
+
+destination_folders = get_all_folders(folder_path)
+for i in range(len(destination_folders)):
+    destination_folders[i] = folder_path + destination_folders[i]
+
+copy_to_multiple_destinations('./solution', destination_folders)
+
+
+def run_python_file_in_folders(file_name, destination_folders):
+    try:
+        for destination_folder in destination_folders:
+            script_path = os.path.join(destination_folder, file_name)
+            
+            if os.path.isfile(script_path) and script_path.endswith(".py"):
+                # Run the Python script
+                current_directory = os.getcwd()
+                os.chdir(destination_folder)
+                
+                os.system("echo $PWD")
+                os.system(f"python3 {file_name}")
+                
+                print('----------------------')
+                print('\n')
+                
+                # Change back to the original working directory
+                os.chdir(current_directory)
+            else:
+                print(f"Script '{file_name}' not found in '{destination_folder}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+run_python_file_in_folders('test.py', destination_folders)
+
+
+
+
+
+
---
+++ unzip.py
@@ -0,0 +1,44 @@
+import os
+import zipfile
+import shutil
+
+def extract_cpp_files(src_folder):
+    for root, dirs, files in os.walk(src_folder):
+        if '__MACOSX' in dirs:
+            macosx_folder = os.path.join(root, '__MACOSX')
+            shutil.rmtree(macosx_folder)
+            print(f"Deleted: {macosx_folder}")
+    for root, dirs, files in os.walk(src_folder):
+        for file_name in files:
+            if file_name.lower().endswith('.cpp') and 'main' in file_name:
+                file_path = os.path.join(root, file_name)
+                if file_path != src_folder + '/' + file_name:
+                    shutil.copy2(file_path, src_folder)
+                
+def unzip_all_files(parent_folder, extract_folder):
+    for root, dirs, files in os.walk(parent_folder):
+        for file_name in files:
+            if file_name.lower().endswith('.zip'):
+                zip_path = os.path.join(root, file_name)
+                unzip_path = os.path.join(extract_folder, os.path.relpath(zip_path, parent_folder).replace('.zip', ''))
+                
+                # Create the destination folder if it doesn't exist
+                os.makedirs(unzip_path, exist_ok=True)
+
+                # Extract the contents of the zip file
+                with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                    zip_ref.extractall(unzip_path)
+    
+
+# Replace 'your_directory_path' with the path to your folder containing zip folders
+directory_path = 'submissions_PA0'
+dest_path = 'submissions_PA0/extract'
+unzip_all_files(directory_path, dest_path)
+
+all_items = os.listdir(dest_path)
+top_level_directories = [item for item in all_items if os.path.isdir(os.path.join(dest_path, item))]
+
+top_level_directories.sort()
+for topdir in top_level_directories:
+    if not topdir == '__MACOSX':
+        extract_cpp_files(dest_path + '/' + topdir)
